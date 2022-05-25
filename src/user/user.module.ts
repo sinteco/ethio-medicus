866faa30eaab0a7a8a95654e6b4900fda2ba7a7c
sinteco <centg2g@gmail.com>
2022-05-25 21:38:27 +0300

    naming fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
 import { UsersService } from './service/users/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { userschema } from './schema/user.schema';
+import { UsersController } from './controller/users.controller';
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'user', schema: userschema }])],
   controllers: [UsersController],
